3aa3e6cf760f0f1be944b4ac8a55d60eb0465047
Laurent Bassin <laurent@bassin.info>
2018-07-20 20:04:41 +0200

    Add prefix to api routes

--- src/Controller/PlanetController.php
+++ src/Controller/PlanetController.php
@@ -5,15 +5,17 @@ namespace App\Controller;
 use App\Entity\Planet;
 use App\Repository\PlanetRepository;
 use Doctrine\ORM\ORMException;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\View\View;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 /**
  * @author Laurent Bassin <laurent@bassin.info>
+ *
+ * @Rest\Route("/api")
  */
 class PlanetController extends FOSRestController
 {
